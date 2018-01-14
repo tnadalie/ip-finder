567ab3e45f7920402b53c2114c3ab727752fddca
tnadalie <t.nadalie33@gmail.com>
2018-01-14 17:44:51 -0600

    add argument null value check

--- argv_parser.py
+++ argv_parser.py
@@ -1,5 +1,6 @@
 #/usr/bin/en python
 import argparse
+import sys
 
 def findip_argparser():
     parser = argparse.ArgumentParser(prog='./ipfinder.sh', description='Python module to retrieve postal address from IP/Domain Name.')
@@ -7,4 +8,12 @@ def findip_argparser():
     parser.add_argument('-d', '--domain', nargs='?', help='Host Target Domain Name')
     parser.add_argument('-i', '--ip', nargs='?', help='Host Target IP')
     args = parser.parse_args()
-    return args
+
+    if args.domain is None:
+        print("[*] [-d] <-> [--domain] argument requires a value.")
+    elif args.ip is None:
+        print("[*] [-i] <-> [--ip] argument requires a value.")
+    else:
+        return args
+    print("[*] Application Shutting Down.")
+    sys.exit(1)
