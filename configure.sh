c1ad72fb8ee0fae3af719e6a109be3ce14186f92
tnadalie <t.nadalie33@gmail.com>
2018-01-14 17:45:30 -0600

    add rpm/apt-get verification

--- configure.sh
+++ configure.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 echo -e "[*] Starting ...\n"
+
+# check if distrib is centos
+OS=[[ command -v yum ]]
+
 echo -e "[*] Installing dependencies ...\n"
-sudo -H apt-get install python-pip python-dev build-essential
-sudo -H pip install --upgrade pip setuptools
-sudo -H pip install pygeoip wget requests
+if [ ${#OS} -ge 1 ]; then
+  sudo -H yum install -y epel-release
+  sudo -H yum update -y
+  sudo -H yum install -y python-pip python-dev build-essential
+  sudo -H pip install --upgrade pip setuptools
+  sudo -H pip install pygeoip wget requests
+else
+  sudo -H apt-get update -y & sudo -H apt-get upgrade -y
+  sudo -H apt-get install -y python-pip python-dev build-essential
+  sudo -H pip install --upgrade pip setuptools
+  sudo -H pip install pygeoip wget requests
+fi
 echo -e "\n[*] Script finished ..."
